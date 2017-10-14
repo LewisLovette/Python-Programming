dcbf5fa1d8ee5a1e966cb74cd98bf2c8c825a6e9
LewisLovette <lewislovette3@hotmail.co.uk>
2017-10-14 23:15:31 +0100

    Program was not gravy today, close on eulerP20 but LS3q2 doesn't make sense, the brief MUST be wrong. Oh well at least I plowed through loads of maths today :D

--- EulerProblems/eulerP20.py
+++ EulerProblems/eulerP20.py
@@ -1,18 +1,12 @@
+from math import factorial
 #Find the sum of the digits in the number 100!
-x = 99 #since we start on 100
-sum100 = 100
+xFact = factorial(100)
 sumOfDigits = 0
 
-while x != 1:
-	sum100 *= x
-	print(str(sum100)+" - "+str(x))
-	x -= 1
+while(xFact > 0):
+	sumOfDigits += xFact % 10
+	xFact /= 10
+	round(xFact, 0)
+	print(str(xFact)+"\n")
 
-x = 100
-
-while x != 0:
-	sumOfDigits += sum100 % 10
-	sum100 /= 10
-	x -= 1
-
-print("The sum of digits for 100! is: "+str(sumOfDigits))
+print("The sum of the digits for 100! is: "+str(sumOfDigits)+"\nFactorial: "+str(xFact))
