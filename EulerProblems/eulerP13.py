6317f206d196bc24e645d340e556f10925600e83
LewisLovette <lewislovette3@hotmail.co.uk>
2017-10-19 21:52:32 +0100

    Did problem 10, pretty nice once you think about it. Getting back into euler :D

---
+++ EulerProblems/eulerP13.py
@@ -0,0 +1,1 @@
+#Solve the sum of the first 10 digits in this number:
--- EulerProblems/eulerP20.py
+++ EulerProblems/eulerP20.py
@@ -1,17 +1,12 @@
 from math import factorial
 #Find the sum of the digits in the number 100!
-xFact = factorial(100)
+xFact = str(factorial(100))
 sumOfDigits = 0
-randList = []
+xFactArray = list(xFact)	#puts each number in 100! into an array of each character
+print(str(xFactArray))	#test
 
-for i in len(xFact):
-	randList[i] = xFact[i]
-	sumOfDigits += xFact[i]
+for i in xFactArray:
+	sumOfDigits += int(i)
 
-#while(xFact > 0):
-#	sumOfDigits += xFact % 10
-#	xFact /= 10
-#	round(xFact, 0)
-#	print(str(xFact)+"\n")
-
-print("The sum of the digits for 100! is: "+str(sumOfDigits)+"\nFactorial: "+str(xFact))
+print("\nFactorial: "+str(xFact))	#test
+print("\nThe sum of the digits for 100! is: "+str(sumOfDigits))
