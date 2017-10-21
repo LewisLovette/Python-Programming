e763bd57cf0217c42294ddeb274101724be8a120
LewisLovette <lewislovette3@hotmail.co.uk>
2017-10-21 22:28:55 +0100

    Turns out I read it wrong so this shouldn't be too difficult now! too bad I spent quite a few hours on the wrong version of this oops.

--- LabSheetQuestions/LabSheet4ExtendedTask/testingFunctions.py
+++ LabSheetQuestions/LabSheet4ExtendedTask/testingFunctions.py
@@ -13,56 +13,19 @@ import datetime as dt
 #--------------------------------------------------------------------------------------------------------------
 #Tesco: https://www.quandl.com/data/LSE/TSCO-TESCO-share-price-TSCO-Currency-GBX
 
-
-randList = list(GetFromQuandl("LSE", "TSCO"))	#Make it print a little neater
-years = []
-price = []
-countX = 0
-dateCount = 0
-for i in randList:
-	for j in i:
-		if countX == 0:
-			date = j[0:4]+j[5:7]+j[8:10]
-			date = int(date)
-			#print(str(date))
-			years.append(date)
-		elif countX == 1:
-			price.append(j)
-		
-		countX +=1
-		#print(str(j)+"\t", end="")
-	
+def stockQuandl(country, company):
+	randList = list(GetFromQuandl(country, company))	#Make it print a little neater
+	price = []
 	countX = 0
-	#print("\n")
+	for i in randList:
+		price.append(i[1])
+		
+	return(price)
 
-date_fmt = '%Y%m%d'
-dt_x = [dt.datetime.strptime(str(i), date_fmt) for i in years]
-x = [dates.date2num(i) for i in dt_x]
+tesco = stockQuandl("LSE", "TSCO")
+sainsburys = stockQuandl("LSE", "SBRY")
+morrisons = stockQuandl("LSE", "MRW")
+averages = []
 
-fig, ax = plt.subplots()
-
-# Use plot_date rather than plot when dealing with time data.
-ax.plot_date(years, price, 'bo-')
-
-date_formatter = dates.DateFormatter('%Y%m%d')
-
-ax.xaxis.set_major_formatter(date_formatter)
-
-fig.autofmt_xdate()
-
-plt.show()
-
-#print("Year:\n"+str(years)+"\n\n\n")
-#yearsPlot = range(2006, 2018)
-#pricePlot = range(100, 500)
-#plt.plot_date(years, price, fmt="o", xdate=True, ydate=False)
-
-#plt.plot(years, price, 'ro')
-
-#plt.gca().xaxis.set_major_formatter(dates.DateFormatter("%Y%m%d"))
-#plt.gca().xaxis.set_major_locator(dates.DayLocator())
-#plt.gfc().autofmt_xdate()
-
-#plt.ylabel("Testing")
-#plt.show()
-
+for i in range(len(tesco)):		#range for the length tesco[]
+	#print(str(i))
