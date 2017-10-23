2f2290e635d33d98280381914be981a1b95e14b9
LewisLovette <lewislovette3@hotmail.co.uk>
2017-10-23 14:48:07 +0100

    This will do for now, love doing anything GUI so this was a nice introduction but will start next weeks work for now.

--- LabSheetQuestions/LabSheet4ExtendedTask/testingFunctions.py
+++ LabSheetQuestions/LabSheet4ExtendedTask/testingFunctions.py
@@ -1,8 +1,6 @@
 from WorkingWithQuandl import *
+from statistics import stdev
 import matplotlib.pyplot as plt
-import matplotlib.dates as dates
-import numpy as np
-import datetime as dt
 #-------------------------------------------------------------------------------------------------------------
 #Use the function to gather share data from the London Stock Exchange for three of the biggest UK
 #supermarkets: Tesco, Sainsburys and Morrisons. Write you own code to compare the stock price levels and
@@ -14,7 +12,7 @@ import datetime as dt
 #Tesco: https://www.quandl.com/data/LSE/TSCO-TESCO-share-price-TSCO-Currency-GBX
 
 def stockQuandl(country, company):
-	randList = list(GetFromQuandl(country, company))	#Make it print a little neater
+	randList = list(GetFromQuandl(country, company))
 	price = []
 	countX = 0
 	for i in randList:
@@ -22,10 +20,53 @@ def stockQuandl(country, company):
 		
 	return(price)
 
+def average(stock):
+	average = 0
+	for i in stock:
+		average += int(i)
+
+	average /= len(stock)
+	average = round(average, 2)	#since it doesn't round it when I wrap it around 'len(stock'
+
+	print("\n\n"+str(average))
+
+	return(average)
+
+def standardDeviation(list):
+	stanDev = round(stdev(list), 2)
+	print("\n\n"+str(stanDev))
+
+	return(stanDev)
+
+def flip(stock):
+	stockFlip = list(reversed(stock))
+	return(stockFlip)
+
 tesco = stockQuandl("LSE", "TSCO")
 sainsburys = stockQuandl("LSE", "SBRY")
 morrisons = stockQuandl("LSE", "MRW")
-averages = []
 
-for i in range(len(tesco)):		#range for the length tesco[]
+#print("Tesco:\n"+str(tesco)+"\n\n\nSainsburys\n"+str(sainsburys)+"\n\n\nMorrisons:\n"+str(morrisons))
+#for i in range(len(tesco)):		#range for the length tesco[]
 	#print(str(i))
+
+
+average(tesco)
+standardDeviation(tesco)
+
+tescoRange = len(tesco)
+print("\n\n"+str(tescoRange))
+
+plt.gca().set_color_cycle(['red', 'green', 'blue'])
+
+plt.plot(range(len(tesco)), flip(tesco), linewidth=1.3)
+plt.plot(range(len(sainsburys)), flip(sainsburys), linewidth=1.3)
+plt.plot(range(len(morrisons)), flip(morrisons), linewidth=1.3)
+
+plt.ylabel("Stock Prices (GBP)")
+plt.xlabel("Time: Days (2006 - 2017)")
+
+plt.legend(['Tesco', 'Sainsburys', 'Morrisons'], loc='upper left')
+plt.title("Tesco, Sainsburys and Morrisons Stock Prices: 2006-2017")
+
+plt.show()
